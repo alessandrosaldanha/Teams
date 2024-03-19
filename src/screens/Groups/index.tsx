c74bde1df9c1ea5e5041fc27a36e8169a1e279a3
alessandro saldanha <alessandrosaldanha.as@gmail.com>
2024-03-19 11:16:12 -0300

    "criação"

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,21 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Gruops() {
+    return (
+        <View style={styles.container}>
+            <Text>Groups</Text>
+        </View>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+});
