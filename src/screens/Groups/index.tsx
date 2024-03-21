3535c56f2ce037d5200e4db0b3ba5ed45dd5c019
alessandro saldanha <alessandrosaldanha.as@gmail.com>
2024-03-21 12:14:31 -0300

    "progresso"

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,18 @@
 import { Header } from '@components./Header';
+import { Highlight } from '@components./Highlight';
+import { GruopCard } from '@components./GroupCard';
+
 import { Container } from './styles';
 
 export function Gruops() {
     return (
     <Container>
         <Header />
+        <Highlight 
+            title="Turmas"
+            subtitle="jogue com a sua turma"
+        />
+        <GruopCard title="Galera do Ignite" />
     </Container>
     );
 }
