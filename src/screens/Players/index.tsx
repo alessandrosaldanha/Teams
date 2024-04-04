4c52f0a166db23976d25e2a6eb35bb3f56218da0
Alessandro Saldanha <alessandro.saldanha@orla.tech>
2024-04-04 18:14:34 -0300

    "finalizando"

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useRoute } from "@react-navigation/native";
 import { FlatList } from "react-native";
 import { Input } from "@components./Input";
 import { Filter } from "@components./Filter";
@@ -9,23 +10,25 @@ import { Highlight } from "@components./Highlight";
 import { PlayerCard } from "@components./PlayersCard";
 import { ButtonIcon } from "@components./Buttonicon";
 
-
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
-
-
-
+type RouteParams = {
+    group: string;
+}
 
 export function Players() { 
     const [team, setTeam] = useState('Time A');
     const [players, setPlayers] = useState([]);
+
+    const route = useRoute();
+    const { group } = route.params as RouteParams;
 
     return (
         <Container>
             <Header showBackButton />
 
             <Highlight 
-                title="Nome da turma"
+                title={group}
                 subtitle="adicione a galera e separe os times"
             />
         <Form>
